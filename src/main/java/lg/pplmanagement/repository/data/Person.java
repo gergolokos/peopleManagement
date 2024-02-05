f58bebb82e653eea4f0a7aeb1319654e182c596e
GergoLokos <lokosgergo@icloud.com>
2024-02-05 23:34:02 +0100

    Creating Person entity

--- src/main/java/lg/pplmanagement/repository/data/Person.java
+++ src/main/java/lg/pplmanagement/repository/data/Person.java
@@ -1,4 +1,66 @@
 package lg.pplmanagement.repository.data;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity(name = "Person")
+@Table(name = "person")
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "dob")
+    private LocalDate dob;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public Person() {
+    }
+
+    public Person(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.dob = builder.dob;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String name;
+        private LocalDate dob;
+
+        public Builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setDob(LocalDate dob) {
+            this.dob = dob;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
 }
