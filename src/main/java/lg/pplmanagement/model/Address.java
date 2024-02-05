aa6ec153b237e15770331f9c6c3fdf03d18a4a6d
GergoLokos <lokosgergo@icloud.com>
2024-02-05 23:54:42 +0100

    Creating Address model class

--- src/main/java/lg/pplmanagement/model/Address.java
+++ src/main/java/lg/pplmanagement/model/Address.java
@@ -1,4 +1,79 @@
 package lg.pplmanagement.model;
 
+import java.util.Objects;
+
 public class Address {
+    private final Long id;
+    private final String permanentAddress;
+    private final String temporaryAddress;
+
+    private final Person person;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPermanentAddress() {
+        return permanentAddress;
+    }
+
+    public String getTemporaryAddress() {
+        return temporaryAddress;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    Address(Builder builder) {
+        this.id = builder.id;
+        this.permanentAddress = builder.permanentAddress;
+        this.temporaryAddress = builder.temporaryAddress;
+        this.person = builder.person;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String permanentAddress;
+        private String temporaryAddress;
+        private Person person;
+
+        public Builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setPermanentAddress(String permanentAddress) {
+            this.permanentAddress = permanentAddress;
+            return this;
+        }
+
+        public Builder setTemporaryAddress(String temporaryAddress) {
+            this.temporaryAddress = temporaryAddress;
+            return this;
+        }
+
+        public Builder setPerson(Person person) {
+            this.person = person;
+            return this;
+        }
+
+        public Address build() {
+            return new Address(this);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Builder builder = (Builder) o;
+            return Objects.equals(id, builder.id) && Objects.equals(permanentAddress, builder.permanentAddress) && Objects.equals(temporaryAddress, builder.temporaryAddress) && Objects.equals(person, builder.person);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, permanentAddress, temporaryAddress, person);
+        }
+    }
 }
+
