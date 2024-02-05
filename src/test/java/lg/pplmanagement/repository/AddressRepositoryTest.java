020df48ac2622251de6c5c2e1c2175a2c41b79d7
GergoLokos <lokosgergo@icloud.com>
2024-02-06 00:41:20 +0100

    Creating RepositoryTest test classes

---
+++ src/test/java/lg/pplmanagement/repository/AddressRepositoryTest.java
@@ -0,0 +1,20 @@
+package lg.pplmanagement.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressRepositoryTest {
+
+    @Test
+    void updateAddress() {
+    }
+
+    @Test
+    void countAddressWithoutTemporary() {
+    }
+
+    @Test
+    void countAddressWithTemporary() {
+    }
+}
---
+++ src/test/java/lg/pplmanagement/repository/ContactRepositoryTest.java
@@ -0,0 +1,20 @@
+package lg.pplmanagement.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactRepositoryTest {
+
+    @Test
+    void updateContact() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void findByPhoneNumber() {
+    }
+}
