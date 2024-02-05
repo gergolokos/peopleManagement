a0ae63b1360284137c68df239ca33505d74b9bf1
GergoLokos <lokosgergo@icloud.com>
2024-02-06 00:36:57 +0100

    Creating additional validatons on AddressValidationService

--- src/main/java/lg/pplmanagement/service/AddressValidationService.java
+++ src/main/java/lg/pplmanagement/service/AddressValidationService.java
@@ -1,7 +1,32 @@
 package lg.pplmanagement.service;
 
+import lg.pplmanagement.api.rest.Address;
+import lg.pplmanagement.repository.AddressRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressValidationService {
+
+    private final AddressRepository addressRepository;
+
+    public AddressValidationService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    public boolean isAddressValid(Address address) {
+        return isPermanentAddressValid(address.getPermanentAddress()) && isAddressNumberValid(address);
+    }
+
+    private boolean isPermanentAddressValid(String permanent) {
+        return permanent != null && !permanent.isEmpty();
+    }
+
+    private boolean isAddressNumberValid(Address address) {
+
+        int addressWithoutTemporary = (int) addressRepository.countAddressWithoutTemporary(address.getPersonId());
+        int addressWithTemporary = (int) addressRepository.countAddressWithTemporary(address.getPersonId());
+
+        return addressWithoutTemporary + addressWithTemporary < 1;
+    }
+
 }
