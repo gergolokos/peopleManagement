8c318cbe07f85e23c5f9cb641013dc7a8af8b2d5
GergoLokos <lokosgergo@icloud.com>
2024-02-05 23:47:06 +0100

    Implementing convertToModel and convertToEntity methods

--- src/main/java/lg/pplmanagement/repository/converter/PersonRepositoryConverter.java
+++ src/main/java/lg/pplmanagement/repository/converter/PersonRepositoryConverter.java
@@ -1,7 +1,25 @@
 package lg.pplmanagement.repository.converter;
 
+import lg.pplmanagement.repository.data.Person;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PersonRepositoryConverter {
+public class PersonRepositoryConverter extends RepositoryConverter<Person, lg.pplmanagement.model.Person> {
+    @Override
+    public lg.pplmanagement.model.Person convertToModel(Person entity) {
+        return new lg.pplmanagement.model.Person.Builder()
+                .setId(entity.getId())
+                .setName(entity.getName())
+                .setDob(entity.getDob())
+                .build();
+    }
+
+    @Override
+    public Person convertToEntity(lg.pplmanagement.model.Person model) {
+        return new Person.Builder()
+                .setId(model.getId())
+                .setName(model.getName())
+                .setDob(model.getDob())
+                .build();
+    }
 }
