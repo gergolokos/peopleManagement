8efe09bb8454261ef1198bc7da2b59cd14316b7e
GergoLokos <lokosgergo@icloud.com>
2024-02-05 23:59:27 +0100

    Creating Contact api class

--- src/main/java/lg/pplmanagement/api/rest/Contact.java
+++ src/main/java/lg/pplmanagement/api/rest/Contact.java
@@ -1,59 +1,62 @@
 package lg.pplmanagement.api.rest;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import java.util.Objects;
 
+@JsonDeserialize(builder = Contact.Builder.class)
 public class Contact {
-    private final Long id;
+    @JsonProperty("email")
     private final String email;
+    @JsonProperty("phoneNumber")
     private final String phoneNumber;
-    private final Person person;
+    @JsonProperty("personId")
+    private final Long personId;
 
-    public Long getId() {
-        return id;
-    }
-
+    @JsonProperty("email")
     public String getEmail() {
         return email;
     }
 
+    @JsonProperty("phoneNumber")
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public Person getPerson() {
-        return person;
+    @JsonProperty("personId")
+    public Long getPersonId() {
+        return personId;
     }
 
-    public Contact(Builder builder) {
-        this.id = builder.id;
+    private Contact(Builder builder) {
         this.email = builder.email;
         this.phoneNumber = builder.phoneNumber;
-        this.person = builder.person;
+        this.personId = builder.personId;
     }
 
     public static class Builder {
-        private Long id;
+        @JsonProperty("email")
         private String email;
+        @JsonProperty("phoneNumber")
         private String phoneNumber;
-        private Person person;
+        private Long personId;
 
-        public Builder setId(Long id) {
-            this.id = id;
-            return this;
-        }
-
+        @JsonProperty("email")
         public Builder setEmail(String email) {
             this.email = email;
             return this;
         }
 
+        @JsonProperty("phoneNumber")
         public Builder setPhoneNumber(String phoneNumber) {
             this.phoneNumber = phoneNumber;
             return this;
         }
 
-        public Builder setPerson(Person person) {
-            this.person = person;
+        @JsonProperty("personId")
+        public Builder setPersonId(Long personId) {
+            this.personId = personId;
             return this;
         }
 
@@ -66,12 +69,12 @@ public class Contact {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             Builder builder = (Builder) o;
-            return Objects.equals(id, builder.id) && Objects.equals(email, builder.email) && Objects.equals(phoneNumber, builder.phoneNumber) && Objects.equals(person, builder.person);
+            return Objects.equals(email, builder.email) && Objects.equals(phoneNumber, builder.phoneNumber) && Objects.equals(personId, builder.personId);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(id, email, phoneNumber, person);
+            return Objects.hash(email, phoneNumber, personId);
         }
     }
 }
