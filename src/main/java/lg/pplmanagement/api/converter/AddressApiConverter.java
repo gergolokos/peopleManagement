f5c5e0386653e23a5fd6ab5a004ddc692b6e5276
GergoLokos <lokosgergo@icloud.com>
2024-02-06 00:18:41 +0100

    Implementing convertToModel and convertToApi methods on AddressApiConverter

--- src/main/java/lg/pplmanagement/api/converter/AddressApiConverter.java
+++ src/main/java/lg/pplmanagement/api/converter/AddressApiConverter.java
@@ -1,7 +1,38 @@
 package lg.pplmanagement.api.converter;
 
+import lg.pplmanagement.api.rest.Address;
+import lg.pplmanagement.repository.PersonRepository;
+import lg.pplmanagement.repository.converter.PersonRepositoryConverter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AddressApiConverter {
+public class AddressApiConverter extends ApiConverter<Address, lg.pplmanagement.model.Address> {
+
+    private final PersonRepositoryConverter personRepositoryConverter;
+    private final PersonRepository personRepository;
+
+    public AddressApiConverter(PersonRepositoryConverter personRepositoryConverter, PersonRepository personRepository) {
+        this.personRepositoryConverter = personRepositoryConverter;
+        this.personRepository = personRepository;
+    }
+
+    @Override
+    public lg.pplmanagement.model.Address convertToModel(Address api) {
+        return new lg.pplmanagement.model.Address.Builder()
+                .setPermanentAddress(api.getPermanentAddress())
+                .setTemporaryAddress(api.getTemporaryAddress())
+                .setPerson(personRepositoryConverter.convertToModel(personRepository.findById(api.getPersonId().intValue())
+                        .orElseThrow(() -> new RuntimeException("Person not found for: " + api.getPersonId()))
+                ))
+                .build();
+    }
+
+    @Override
+    public Address convertToApi(lg.pplmanagement.model.Address model) {
+        return new Address.Builder()
+                .setPermanentAddress(model.getPermanentAddress())
+                .setTemporaryAddress(model.getTemporaryAddress())
+                .setPersonId(model.getId())
+                .build();
+    }
 }
