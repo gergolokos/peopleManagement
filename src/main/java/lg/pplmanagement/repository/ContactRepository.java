44d452f1b45afda4100a57658f6f19c1bb18e3a1
GergoLokos <lokosgergo@icloud.com>
2024-02-06 00:29:24 +0100

    Creating updateContact, findByEmail and findByPhoneNumber method

--- src/main/java/lg/pplmanagement/repository/ContactRepository.java
+++ src/main/java/lg/pplmanagement/repository/ContactRepository.java
@@ -1,9 +1,28 @@
 package lg.pplmanagement.repository;
 
+import jakarta.transaction.Transactional;
 import lg.pplmanagement.repository.data.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
+    @Transactional
+    @Modifying
+    @Query("UPDATE Contact c SET c.email = :email, c.phoneNumber = :phoneNumber WHERE c.id = :id")
+    void updateContact(@Param("id") Integer id,
+                       @Param("email") String email,
+                       @Param("phoneNumber") String phoneNumber
+    );
+
+    @Query("SELECT c FROM Contact c WHERE c.email = :email")
+    Optional<Contact> findByEmail(@Param("email") String email);
+
+    @Query("SELECT c FROM Contact c WHERE c.phoneNumber = :phoneNumber")
+    Optional<Contact> findByPhoneNumber(@Param("phoneNumber") String phoneNumber);
 }
