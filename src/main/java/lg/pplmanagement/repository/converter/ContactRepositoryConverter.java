38217f9127d8dd4eb8054a2bee00508f62734f9c
GergoLokos <lokosgergo@icloud.com>
2024-02-05 23:53:33 +0100

    Implementing convertToModel and convertToEntity methods on ContactRepositoryConverter

--- src/main/java/lg/pplmanagement/repository/converter/ContactRepositoryConverter.java
+++ src/main/java/lg/pplmanagement/repository/converter/ContactRepositoryConverter.java
@@ -1,7 +1,35 @@
 package lg.pplmanagement.repository.converter;
 
+import lg.pplmanagement.repository.data.Contact;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ContactRepositoryConverter {
+public class ContactRepositoryConverter extends RepositoryConverter<Contact, lg.pplmanagement.model.Contact> {
+
+    private final PersonRepositoryConverter personRepositoryConverter;
+
+    ContactRepositoryConverter(PersonRepositoryConverter personRepositoryConverter) {
+        this.personRepositoryConverter = personRepositoryConverter;
+    }
+
+    @Override
+    public lg.pplmanagement.model.Contact convertToModel(Contact entity) {
+        return new lg.pplmanagement.model.Contact.Builder()
+                .setId(entity.getId())
+                .setEmail(entity.getEmail())
+                .setPhoneNumber(entity.getPhoneNumber())
+                .setPerson(personRepositoryConverter.convertToModel(entity.getPerson()))
+                .build();
+    }
+
+    @Override
+    public Contact convertToEntity(lg.pplmanagement.model.Contact model) {
+        return new Contact.Builder()
+                .setId(model.getId())
+                .setEmail(model.getEmail())
+                .setPhoneNumber(model.getPhoneNumber())
+                .setPerson(personRepositoryConverter.convertToEntity(model.getPerson()))
+                .build();
+    }
+    
 }
