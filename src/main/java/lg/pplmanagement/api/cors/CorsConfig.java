f2a5549bb9a4f3dd537a4f20acac1000d209148d
GergoLokos <lokosgergo@icloud.com>
2024-02-05 23:31:08 +0100

    Implementing CorsConfig

--- src/main/java/lg/pplmanagement/api/cors/CorsConfig.java
+++ src/main/java/lg/pplmanagement/api/cors/CorsConfig.java
@@ -1,7 +1,23 @@
 package lg.pplmanagement.api.cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
+
+    private static final String BASE_PATH_PATTERN = "/api/v1/**";
+
+    @Value("${cors.enabled.domain}")
+    private String corsEnabledDomain;
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping(BASE_PATH_PATTERN)
+                .allowedOrigins(corsEnabledDomain)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowCredentials(true);
+    }
+
 }
